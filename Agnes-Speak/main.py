99bab4f0c4ec64ea9ff49baa54eb0f642ec43ef4
Guto Leoni <guto-acmilan@hotmail.com>
2016-09-19 23:47:54 -0300

    mudando imagem da interface

--- Agnes-Speak/main.py
+++ Agnes-Speak/main.py
@@ -8,23 +8,34 @@ def work():
 
 class ButtonFrame(wx.Frame):
 	def __init__(self):
-		wx.Frame.__init__(self, None, -1, 'Agnes', size=(410, 310))
+		wx.Frame.__init__(self, None, -1, 'Agnes', size=(510, 330))
 		self.Center()
 		panel = wx.Panel(self, -1)
-		self.button = wx.Button(panel, -1, "Start", pos=(160, 280))
+		self.button = wx.Button(panel, -1, "Start", pos=(205, 300))
 		self.Bind(wx.EVT_BUTTON, self.start, self.button)
 		self.button.SetDefault()
 		
 		#self.label = wx.StaticText(panel, -1, "Oi, eu sou a Agnes ^^", pos=(230,10))
 		#self.texto = wx.TextCtrl(panel, pos=(220,30), size = (180,50))
 		
-		# Image link: http://28.media.tumblr.com/tumblr_lgu4uzyYkB1qe2w1uo1_500.jpg
-		png = wx.Image("Imagem/eva.JPG",wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+		png = wx.Image("Imagem/Faces/face-1.png",wx.BITMAP_TYPE_ANY).ConvertToBitmap()
 		wx.StaticBitmap(panel, 1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
+		
+
+	"""	
+	def change_image(self,number):
+		print "ve "+str(number)
+		panel = wx.Panel(self, -1)
+		png = wx.Image("Imagem/Faces/face-"+str(number)+".png",wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+		wx.StaticBitmap(panel, 1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
+	"""
 
 	def start(self, event):
-		t = threading.Thread(target = work)
-		t.start()
+
+		t1 = threading.Thread(target = work)
+		t1.start()
+
+
 
 
 def main():
